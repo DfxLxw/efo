485650a31d97e5c5cb406d5cd237a802868ea25f
lxw <liuxianweipp@163.com>
2018-05-11 09:53:42 +0800

    0.1 首页调转问题

---
+++ src/main/java/com/zhazhapan/efo/config/WebMvcConfigContro.java
@@ -0,0 +1,20 @@
+package com.zhazhapan.efo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebMvcConfigContro extends WebMvcConfigurerAdapter {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry)
+    {
+        registry.addViewController("/").setViewName("forward:/signin.html");
+
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+        super.addViewControllers(registry);
+        }
+
+}
